409fedbf43138ebff373d35804f8d5a5d769804f
Danylo <danyloboshagora@gmail.com>
2022-10-16 21:02:49 +0200

    Remember npm install

---
+++ npm is the way/app.js
@@ -0,0 +1,18 @@
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import {Font} from "@ckeditor/ckeditor5-font";
+
+ClassicEditor
+    .create( document.querySelector( '#editor' ), {
+        plugins: [ Essentials, Paragraph, Bold, Italic, Font ],
+        toolbar: [ 'bold', 'italic', 'fontColor' ]
+    } )
+    .then( editor => {
+        console.log( 'Editor was initialized', editor );
+    } )
+    .catch( error => {
+        console.error( error.stack );
+    } );
